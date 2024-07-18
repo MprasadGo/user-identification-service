949c90afd999832674eeac914c17e5367ec6d493
MprasadGo <64685307+MprasadGo@users.noreply.github.com>
2024-07-18 16:12:35 +0530

    added changes

--- src/main/java/com/userservice/oauth/UserIdentificationApplication.java
+++ src/main/java/com/userservice/oauth/UserIdentificationApplication.java
@@ -2,10 +2,12 @@ package com.userservice.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class UserIdentificationApplication {
 
 	public static void main(String[] args) {
