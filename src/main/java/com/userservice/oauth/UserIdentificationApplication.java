ccb0df687b59c9a53cef76fbd45feff1d77d0140
MprasadGo <64685307+MprasadGo@users.noreply.github.com>
2024-07-22 16:00:51 +0530

    added tomcat sever config

--- src/main/java/com/userservice/oauth/UserIdentificationApplication.java
+++ src/main/java/com/userservice/oauth/UserIdentificationApplication.java
@@ -2,16 +2,22 @@ package com.userservice.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableDiscoveryClient
-public class UserIdentificationApplication {
+public class UserIdentificationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserIdentificationApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UserIdentificationApplication.class);
+	}
 }
